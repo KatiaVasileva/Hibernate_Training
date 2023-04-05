9ccc61a8a505a1f00e6cafa0ff5a1081cd6d8a4f
katiavasileva <eka.vasileva@gmail.com>
2023-04-06 01:04:07 +0500

    Initial Commit

--- Testing_1/src/main/java/dao/UserDAOImpl.java
+++ Testing_1/src/main/java/dao/UserDAOImpl.java
@@ -1,2 +1,53 @@
-package dao;public class UserDaoImpl {
+package dao;
+
+import models.Auto;
+import models.User;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import utils.HibernateSessionFactoryUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAOImpl implements UserDAO {
+
+
+    public User findById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(User.class, id);
+    }
+
+    public void save(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(user);
+        transaction.commit();
+        session.close();
+    }
+
+    public void update(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.update(user);
+        transaction.commit();
+        session.close();
+    }
+
+    public void delete(User user) {
+        Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(user);
+        transaction.commit();
+        session.close();
+    }
+
+    public Auto findAutoById(int id) {
+        return HibernateSessionFactoryUtil.getSessionFactory().openSession().get(Auto.class, id);
+    }
+
+    public List<User> findAll() {
+        try (Session session = HibernateSessionFactoryUtil.getSessionFactory().openSession()) {
+            List<User> users = session.createQuery("FROM User").list();
+            return users;
+        }
+    }
 }
--- Testing_1/src/main/java/services/UserService.java
+++ Testing_1/src/main/java/services/UserService.java
@@ -1,2 +1,41 @@
-package services;public class UserService {
+package services;
+
+import dao.UserDAO;
+import dao.UserDAOImpl;
+import models.Auto;
+import models.User;
+
+import java.util.List;
+
+public class UserService {
+
+    private UserDAO usersDAO = new UserDAOImpl();
+
+    public UserService() {
+    }
+
+    public User findUser(int id) {
+        return usersDAO.findById(id);
+    }
+
+    public void saveUser(User user) {
+        usersDAO.save(user);
+    }
+
+    public void updateUser(User user) {
+        usersDAO.update(user);
+    }
+
+    public void deleteUser(User user) {
+        usersDAO.delete(user);
+    }
+
+    public List<User> findAllUsers() {
+        return usersDAO.findAll();
+    }
+
+    public Auto findAutoById(int id) {
+        return usersDAO.findAutoById(id);
+    }
+
 }
